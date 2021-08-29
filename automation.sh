0faa5c5c62751f30e038c0f5f29e625a986a9389
root <root@ip-172-31-89-203.ec2.internal>
2021-08-29 23:16:37 +0000

    Automation-v0.2

--- automation.sh
+++ automation.sh
@@ -3,22 +3,39 @@ sudo apt update -y
 
 # to check that apache is installed or not
 if dpkg --get-selections | grep apache2 ; then
-        echo "apache2 is installed"
 if service apache2 status; then
-        echo "service is running"
-        sudo service apache2 restart
+	sudo service apache2 restart
 fi
 else
         sudo apt install apache2
         sudo service apache2 restart
-        echo "apache2 is not installed"
 fi
 
 #creating the tar file in /tmp/
 timestamp=$(date "+%d%m%Y-%H%M%S")
 tar -cvf /tmp/sivasankari-httpd-logs-$timestamp.tar /var/log/apache2/*.log
-echo "file created $timestamp"
 
+
+#to copy the file created to the s3 bucket
 s3_bucket=upgrad-sivasankari
 aws s3 cp /tmp/sivasankari-httpd-logs-${timestamp}.tar s3://${s3_bucket}/
-echo "copied to s3 bucket"
+
+
+#to create bookkeeping record
+if [[ ! -f /var/www/html/inventory.html ]]
+then
+        tabs 20  #the tabsize
+        echo -e  "Log Type\tTime Created\tType\tSize" > /var/www/html/inventory.html
+fi
+sizemb=$(ls -lh /tmp/sivasankari-httpd-logs-${timestamp}.tar | awk '{print $5}')
+#to append the content to the file >> is used
+echo -e "httpd-logs\t$timestamp\ttar\t$sizemb" >> /var/www/html/inventory.html
+
+#to create the cron job that runs for every minute
+if [[ ! -f /etc/cron.d/automation ]]
+then
+	echo "59 * * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+fi
+
+
+
